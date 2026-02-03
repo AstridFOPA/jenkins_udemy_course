57c467e067c60bb80d699f333051a3dc7a7d0713
AstridFOPA <moisefopa@gmail.com>
2026-02-02 22:47:32 -0700

    Adding my name

--- learn-jenkins-app-main/src/App.js
+++ learn-jenkins-app-main/src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          astrid loic deploy
         </a>
       </header>
       <p>
